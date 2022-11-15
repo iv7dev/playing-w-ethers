37148e4c263fa65fe9452ecce3d418d877da6685
iv7dev <iv7.software@gmail.com>
2022-11-15 20:09:12 -0300

    add dotenv

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
+require('dotenv').config();
 const { ethers } = require("ethers");
-// 1st step : connect to provider or metamask
+// 🚀 1st step : connect to provider or metamask
 // connect to metamask
 // const provider = new ethers.providers.Web3Provider(window.ethereum)
 //connect to rpc server
-const rpcUrl = "https://bsc-dataseed.binance.org/"; // bnb
-const contractWallet = "0x96734B59e5C44cCBE026D835e090a96F9f1B69FD";
-const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+// if production not used dotenv
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
 
-// Look up the current block number
+const contractWallet = process.env.ACCOUNT1;
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_BINANCE);
+
+// 🚀 Look up the current block number
 const numberBlock = async () =>{
     nro = await provider.getBlockNumber();
     console.log("🚀 ~~ Bloque Nro :", nro);
 };
 
-// Get the balance of an account (by address or ENS name, if supported by network)
+// 🚀 Get the balance of an account (by address or ENS name, if supported by network)
 const balance = async () =>{
     const bal = await provider.getBalance(contractWallet);
     console.log("🚀 ~~ Balance :", ethers.utils.formatEther(bal));
 };
+
+// 🚀 Send 1 ether to an ens name.
+// const tx = signer.sendTransaction({
+//     to: "ricmoo.firefly.eth",
+//     value: ethers.utils.parseEther("1.0")
+// });
+
+
 
 numberBlock();
 balance();
